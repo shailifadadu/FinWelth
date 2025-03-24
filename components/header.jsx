bc9fb8f6c399f68767fe03b95da0deb1c018f024
Shaily Fadadu <shailifadadu@gmail.com>
2025-03-24 18:58:04 +0530

    connected frontend with DB

--- components/header.jsx
+++ components/header.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md back z-50 border-gray-950 ">
       {/* Navbar */}
@@ -32,7 +34,7 @@ const Header = () => {
             </Link>
 
             <Link href={"/transaction/create"}>
-              <Button className="flex items-center gap-2">
+              <Button className="flex items-center gap-2 bg-black text-white hover:bg-gray-900">
                 <PenBox size={18} />
                 <span className="hidden md:inline">Add Transaction</span>
               </Button>
